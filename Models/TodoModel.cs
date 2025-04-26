8b82e2d980df93120a2176302f21ba15cf9561a4
biswarup-naha <jeeeeet6902@gmail.com>
2025-04-26 19:53:57 +0530

    project init

---
+++ Models/TodoModel.cs
@@ -0,0 +1,30 @@
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TodoApi.Models;
+
+public class TodoModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; set; }
+
+    [BsonElement("title")]
+    public required string Title { get; set; }
+
+    [BsonElement("description")]
+    public string? Description { get; set; }
+
+    [BsonElement("status")]
+    public required string Status { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+
+    [BsonElement("updatedAt")]
+    public DateTime UpdatedAt { get; set; }
+
+    [BsonElement("user")]
+    public required string User { get; set; }
+}
